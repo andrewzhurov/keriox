2128ba9a685910aa8b032212a3b744e0f8539e75
Andrew Zhurov <zhurov.andrew@gmail.com>
2025-05-17 15:08:26 +0300

    add SAID -> IdentifierPrefix converter helper

--- keriox_core/src/prefix/mod.rs
+++ keriox_core/src/prefix/mod.rs
@@ -64,6 +64,12 @@ impl FromStr for IdentifierPrefix {
     }
 }
 
+impl From<SelfAddressingIdentifier> for IdentifierPrefix {
+    fn from(said: SelfAddressingIdentifier) -> Self {
+        Self::self_addressing(said)
+    }
+}
+
 impl CesrPrimitive for IdentifierPrefix {
     fn derivative(&self) -> Vec<u8> {
         match self {
